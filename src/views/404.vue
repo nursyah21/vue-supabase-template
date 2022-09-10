9961be02ab0f68705ef01b3392014778495b3362
nursyahjaya <nursyahjaya@gmail.com>
2022-09-10 11:44:06 +0700

    add 404 route

---
+++ src/views/404.vue
@@ -0,0 +1,13 @@
+<script>
+  import { Modal } from '../components';
+  export default{
+    components:{
+      Modal
+    }
+  }
+  </script>
+  <template>
+    <Modal class="text-gray-500 text-2xl">
+      404 Not Found
+    </Modal>
+  </template>
